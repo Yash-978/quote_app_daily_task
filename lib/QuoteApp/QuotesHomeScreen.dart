9054cc5e31e578b0901fb1cf4407d4a7938b2af7
pnkaj1999 <pankajgit525@gmail.com>
2024-06-04 15:57:56 +0500

    4/6/24

--- lib/QuoteApp/QuotesHomeScreen.dart
+++ lib/QuoteApp/QuotesHomeScreen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:quote_app_daily_task/QuoteApp/quoteModel.dart';
 import 'package:quote_app_daily_task/Utils/QuotesList.dart';
+
 QuoteModel? quoteModel;
 
 class Quote_HomeScreen extends StatefulWidget {
@@ -13,44 +14,66 @@ class Quote_HomeScreen extends StatefulWidget {
 }
 
 class _Quote_HomeScreenState extends State<Quote_HomeScreen> {
-  void initState()
-  {
+  void initState() {
     quoteModel = QuoteModel.toList(QuoteList);
     super.initState();
   }
-  @override
 
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+
+        appBar: AppBar(centerTitle: true,title: Text('Quotes'),
+        leading: Icon(Icons.menu),),
+
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ...List.generate(
+                  quoteModel!.quoteModel_List.length,
+                  (index) => Card(
+                        color: Colors.primaries[
+                            Random().nextInt((Colors.primaries.length))],
+                        child: ListTile(
+                          title:
+                              Text(quoteModel!.quoteModel_List[index].quote!),
+                          subtitle:
+                              Text(quoteModel!.quoteModel_List[index].author!,style: TextStyle(fontSize: 20,color: Colors.white),),
+                        ),
+                      ))
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Random random = Random();
             int x = random.nextInt(quoteModel!.quoteModel_List.length);
-            showDialog(context: context, builder: (context)=> AlertDialog(
-              title:  Column(
-                children: [
-                  Text(quoteModel!.quoteModel_List[x].author!),
-                  Text(quoteModel!.quoteModel_List[x].quote!),
-                ],
-              ),
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  backgroundColor: Colors.accents[
+                  Random().nextInt((Colors.accents.length))],
+                      title: ListTile(
 
-            ));
-          },child: Icon(Icons.add_circle_outline),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ...List.generate(quoteModel!.quoteModel_List.length, (index) => Card(
-                child: Column(
-                  children: [
-                    Text(quoteModel!.quoteModel_List[index].quote!),
-                    Text(quoteModel!.quoteModel_List[index].author!),
-                  ],
-                ),
-              ))
-            ],
-          ),
+                        title: Text(quoteModel!.quoteModel_List[x].author!,style: TextStyle(fontSize: 35,color: Colors.white),),
+                        subtitle: Text(quoteModel!.quoteModel_List[x].quote!,style: TextStyle(fontSize: 25,),),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Cancel')),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Ok')),
+                      ],
+                    ));
+          },
+          child: Icon(Icons.add_circle_outline),
         ),
       ),
     );
