d94d8c4ba4e07cd9c7fa9b9fcad9659f493739c3
Yash-978 <virwalyash@gmail.com>
2024-06-07 02:58:12 +0530

    4/6/24

--- lib/Toggle_List/toggle.dart
+++ lib/Toggle_List/toggle.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:quote_app_daily_task/QuoteApp/QuotesHomeScreen.dart';
+import 'package:quote_app_daily_task/QuoteApp/quoteModel.dart';
+
+import '../Utils/QuotesList.dart';
+
+QuoteModel? quoteModel;
 
 class ToggleList extends StatefulWidget {
   const ToggleList({super.key});
@@ -11,16 +16,24 @@ class ToggleList extends StatefulWidget {
 bool changeToggle = false;
 
 // IconData selected = IconData(Icons.menu)
-Set <String>_selected = {'ListView'};
+Set<String> _selected = {'ListView'};
 
 class _ToggleListState extends State<ToggleList> {
   @override
+  void initState() {
+    super.initState();
+    quoteModel = QuoteModel.toList(QuoteList);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
         title: Text('Toggle'),
         actions: [
-          SegmentedButton(multiSelectionEnabled: false,
+          SegmentedButton(
+            multiSelectionEnabled: false,
             segments: [
               ButtonSegment<String>(
                   value: 'ListView',
@@ -31,11 +44,11 @@ class _ToggleListState extends State<ToggleList> {
                   icon: const Icon(Icons.list_alt)),
               ButtonSegment<String>(
                 value: 'GridView',
-                label: Text('GridView',
-                    style: TextStyle(fontSize: 20)),
+                label: Text('GridView', style: TextStyle(fontSize: 20)),
                 icon: const Icon(Icons.apps_rounded),
               ),
-            ], selected: _selected,
+            ],
+            selected: _selected,
             onSelectionChanged: (Set<String> newselection) {
               setState(() {
                 _selected = newselection;
@@ -43,13 +56,42 @@ class _ToggleListState extends State<ToggleList> {
               });
             },
           ),
-          (changeToggle) ?
-          ListView.builder(itemCount: quoteModel!.quoteModel_List.length,
-            itemBuilder: (BuildContext context, int index) {
+        ],
+      ),
+      body: Stack(
+        children: [
+          (changeToggle)?
+          GridView.builder(
+            itemCount: quoteModel!.quoteModel_List.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1
+            ),
+            itemBuilder: (context, index) => Card(
+              child: ListTile(
+                title: Text(
+                  quoteModel!.quoteModel_List[index].quote!,style: TextStyle(
 
-            },
-
-
+                ),
+                ),
+                subtitle: Text(
+                  quoteModel!.quoteModel_List[index].author!,
+                ),
+              ),
+            ),
+          ):
+          ListView.builder(
+            itemCount: quoteModel!.quoteModel_List.length,
+            itemBuilder: (context, index) => Card(
+              child: ListTile(
+                title: Text(
+                  quoteModel!.quoteModel_List[index].quote!,
+                ),
+                subtitle: Text(
+                  quoteModel!.quoteModel_List[index].author!,
+                ),
+              ),
+            ),
           )
         ],
       ),
@@ -62,3 +104,100 @@ class _ToggleListState extends State<ToggleList> {
     });
   }
 }
+// SegmentedButton(
+//   multiSelectionEnabled: false,
+//   segments: [
+//     ButtonSegment<String>(
+//         value: 'ListView',
+//         label: Text(
+//           'ListView',
+//           style: TextStyle(fontSize: 20),
+//         ),
+//         icon: const Icon(Icons.list_alt)),
+//     ButtonSegment<String>(
+//       value: 'GridView',
+//       label: Text('GridView', style: TextStyle(fontSize: 20)),
+//       icon: const Icon(Icons.apps_rounded),
+//     ),
+//   ],
+//   selected: _selected,
+//   onSelectionChanged: (Set<String> newselection) {
+//     setState(() {
+//       _selected = newselection;
+//       changeToggle = !changeToggle;
+//     });
+//   },
+// ),
+// list view Builder
+// grid view Builder
+
+/*
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text('List & Grid App'),
+        actions: [
+          IconButton.outlined(onPressed: () {
+            setState(() {
+
+              isGrid = !isGrid;
+
+            });
+          }, icon: Icon(Icons.grid_3x3))
+        ],
+      ),
+      body: (isGrid)?GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
+        ),
+        itemCount: l1.length,
+        itemBuilder: (context, index) => Container(
+          margin: EdgeInsets.all(10),
+          width: double.infinity,
+          height: 300,
+          decoration: BoxDecoration(color: Colors.amber),
+          alignment: Alignment.center,
+          child: Text(l1[index].toString(),style: TextStyle(fontSize: 25),),
+        ),
+      ):ListView.builder(
+        itemCount: l1.length,
+        itemBuilder: (context, index) => Container(
+          margin: EdgeInsets.all(10),
+          width: double.infinity,
+          height: 150,
+          decoration: BoxDecoration(color: Colors.amber),
+          alignment: Alignment.center,
+          child: Text(l1[index].toString(),style: TextStyle(fontSize: 25),),
+        ),
+      ),
+    );
+  }
+}
+
+// Single -> Column -> List.generate -> Widget
+
+// ListView
+// GridView
+// ToggleView ;
+
+bool isGrid = false;
+List l1 = List.generate(100, (index) => index+1);*/
